c8ff1140fc4745d172c32e838a33fd7efa39854a
Mohammed Dawood <mahmedtony73@gmail.com>
2021-05-04 07:23:42 +0200

    Update db.config.js
    fixing some errors

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url: 'mongodb://localhost:27017/mall',
-    url: 'mongodb+srv://mohammeddawood:01150673030@cluster0.cnuxy.mongodb.net/test/mall',
+    url: 'mongodb+srv://mohammeddawood:01150673030@cluster0.cnuxy.mongodb.net/mall',
 }
